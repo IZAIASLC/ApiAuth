1157d13876bf873b4ee80743fc7402455611e57e
izaias <izaiaslc@yahoo.com.br>
2016-11-11 20:04:04 -0200

    Versão inicial

---
+++ ApiAuth/Provider/AuthorizationServerProvider.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ApiAuth.Provider
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin",new []{"*"});
+
+            try {
+
+                var user = context.UserName;
+                var password = context.Password;
+
+                if (user != "izaias")
+                {
+                    context.SetError("invalid_grant", "Usuário ou senha inválidos");
+                    return;
+                }
+
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+                identity.AddClaim(new Claim(ClaimTypes.Name, user));
+
+                var roles = new List<string>();
+
+                roles.Add("User");
+
+                foreach (var role in roles)
+                {
+                    identity.AddClaim(new Claim(ClaimTypes.Role, role));
+
+                }
+
+                context.Validated(identity);
+
+            }
+            catch
+            {
+                context.SetError("invalid_grant", "Falha ao autenticar");
+            }
+        }
+    }
+}
---
+++ ApiAuth/Startup.cs
@@ -0,0 +1,45 @@
+﻿using ApiAuth.Provider;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOauth(app);
+            Register(config);
+            
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
+        }
+        public void ConfigureOauth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/api/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(8),
+                Provider = new AuthorizationServerProvider()
+
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+
+        public void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
